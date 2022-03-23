7013574b1835d9110d7e6bf1df67281f651a7936
omarturmun <93262369+omarturmun@users.noreply.github.com>
2022-03-23 13:48:56 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . # . .
-        . . # # .
+        . # # . .
         # # # # #
-        . . # # .
+        . # # . .
         . . # . .
         `)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 50)
@@ -11,16 +11,16 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(2000)
     basic.showLeds(`
         . . # . .
-        . # # . .
+        . . # # .
         # # # # #
-        . # # . .
+        . . # # .
         . . # . .
         `)
     DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 50)
     DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 50)
     basic.pause(1000)
+    DFRobotMaqueenPlus.I2CInit()
 })
-DFRobotMaqueenPlus.I2CInit()
 basic.forever(function () {
 	
 })
